0af92f633d7c20ebc3ab14d04f3735b212a79884
Antonia000 <nicolaenicoleta31@gmail.com>
2023-05-02 22:22:09 +0300

    Added presentational videos

--- src/app/modules/components/portfolio-section/portfolio-section.component.ts
+++ src/app/modules/components/portfolio-section/portfolio-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-coffee-shop',
@@ -10,7 +10,12 @@ export class PortfolioSectionComponent implements OnInit {
   @Input() title: string = '';
   @Input() description: string = '';
   @Input() link: string = '';
+  @Output() clickEvent = new EventEmitter<boolean>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  handleClick() {
+    this.clickEvent.emit(true);
+  }
 }
--- src/app/modules/containers/portfolio/portfolio.component.ts
+++ src/app/modules/containers/portfolio/portfolio.component.ts
@@ -8,9 +8,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PortfolioComponent implements OnInit {
   activedRoute: string = '';
+  videoMode: boolean = false;
+  videoSource: string = '';
   constructor(private activeRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activedRoute = this.activeRoute.pathFromRoot[1].snapshot.url[1].path;
+  }
+  handleVisitTemplate() {
+    const coffeeShopUrl = '/assets/videos/coffee-shop-presentation.mp4';
+    const barberShopUrl = '/assets/videos/barbershop.mp4';
+    this.videoSource =
+      this.activedRoute === 'coffee-shop' ? coffeeShopUrl : barberShopUrl;
+    console.log(this.videoSource);
+    this.videoMode = true;
   }
 }
